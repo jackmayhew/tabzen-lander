25fae3737a6cfac2f9eca784ee5f5269bfa98cf1
jackmayhew <jacksonmayhew9@gmail.com>
2024-11-14 12:11:17 -0500

    meta

--- components/meta/headConfig.js
+++ components/meta/headConfig.js
@@ -4,23 +4,41 @@ export function setupHead(title) {
     htmlAttrs: {
       lang: 'en',
     },
+    // meta: [
+    //   { name: "description", content: "Your tabs, distraction-free. TabZen blocks intrusive ads, giving you a cleaner view of your guitar, bass, and drum tabs." },
+    //   { charset: "UTF-8" },
+    //   { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+    //   // open graph / facebook
+    //   { property: "og:type", content: "website" },
+    //   { property: "og:site_name", content: "TabZen" },
+    //   { property: "og:url", content: "https://tabzen.app/" },
+    //   { property: "og:title", content: "TabZen" },
+    //   { property: "og:description", content: "Your tabs, distraction-free. TabZen blocks intrusive ads, giving you a cleaner view of your guitar, bass, and drum tabs." },
+    //   { property: "og:image", content: "https://tabzen.app/logo/logo.svg" },
+    //   // twitter
+    //   { property: "twitter:card", content: "summary_large_image" },
+    //   { property: "twitter:url", content: "https://tabzen.app/" },
+    //   { property: "twitter:title", content: "TabZen" },
+    //   { property: "twitter:description", content: "Your tabs, distraction-free. TabZen blocks intrusive ads, giving you a cleaner view of your guitar, bass, and drum tabs." },
+    //   { property: "twitter:image", content: "https://tabzen.app/logo/logo.svg" }
+    // ],
     meta: [
-      { name: "description", content: "Your tabs, distraction-free. TabZen blocks intrusive ads, giving you a cleaner view of your guitar, bass, and drum tabs." },
+      { name: "description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop." },
       { charset: "UTF-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1.0" },
       // open graph / facebook
       { property: "og:type", content: "website" },
-      { property: "og:site_name", content: "TabZen" },
-      { property: "og:url", content: "https://tabzen.app/" },
-      { property: "og:title", content: "TabZen" },
-      { property: "og:description", content: "Your tabs, distraction-free. TabZen blocks intrusive ads, giving you a cleaner view of your guitar, bass, and drum tabs." },
-      { property: "og:image", content: "https://tabzen.app/logo/logo.svg" },
+      { property: "og:site_name", content: "Jack Mayhew" },
+      { property: "og:url", content: "https://jackmayhew.com" },
+      { property: "og:title", content: "Jack Mayhew" },
+      { property: "og:description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop." },
+      { property: "og:image", content: "https://jackmayhew.com/img/og.png" },
       // twitter
       { property: "twitter:card", content: "summary_large_image" },
-      { property: "twitter:url", content: "https://tabzen.app/" },
-      { property: "twitter:title", content: "TabZen" },
-      { property: "twitter:description", content: "Your tabs, distraction-free. TabZen blocks intrusive ads, giving you a cleaner view of your guitar, bass, and drum tabs." },
-      { property: "twitter:image", content: "https://tabzen.app/logo/logo.svg" }
+      { property: "twitter:url", content: "https://jackmayhew.com" },
+      { property: "twitter:title", content: "Jack Mayhew" },
+      { property: "twitter:description", content: "Just a guy who enjoys building cool stuff for the web, making things (blazingly) fast, and ricing my desktop." },
+      { property: "twitter:image", content: "https://jackmayhew.com/img/twitter.png" }
     ],
     link: [
       // favicons
